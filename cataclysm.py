7974f23362599a16b58ce9cd9e30413bab52d1bb
Octav Sandulescu <github@narc.ro>
2015-02-16 12:59:39 +0000

    Add item objects and improve parsing.
    Still all very ad-hoc and barely tested, but the bits the doc mentions
    still match.

--- cataclysm.py
+++ cataclysm.py
@@ -1,45 +1,50 @@
 from __future__ import print_function
 import parse
 
+
+class Item(object):
+    def __init__(self, json):
+        # Sanity check
+        if 'type' not in json or json['type'] not in item_otypes:
+            raise TypeError("Not an item type", json)
+
+        parse.require_all(json, ('id', 'volume', 'weight'))
+
+        self.itype_id = json['id']
+        self.volume = parse.value_from(json, 'volume')
+        self.weight = parse.value_from(json, 'weight')
+        self.default_charges = parse.value_from(json, 'count', 1)
+
+    def __repr__(self):
+        return '<Item(%s)>' % self.itype_id
+
+
+
+item_otypes = ['AMMO', 'ARMOR', 'BIONIC_ITEM', 'BOOK', 'COMESTIBLE', 'CONTAINER', 'GENERIC',
+              'GUN', 'GUNMOD', 'TOOL', 'TOOL_ARMOR', 'VAR_VEH_PART']
+objectifiers = dict.fromkeys(item_otypes, Item)
+
+def objectify(categorized_json):
+    obj_dict = {}
+    for otype, objects in categorized_json.items():
+        if otype not in objectifiers:
+            continue
+        objectify = objectifiers[otype]
+        obj_dict[otype] = []
+
+        for obj in objects:
+            obj_dict[otype].append(objectify(obj))
+    return obj_dict
+
+
 all_json = parse.read_full_json("json/")
+categories = parse.categorize(all_json)
+json = parse.group(categories)
+objects = objectify(categories)
 
-object_types = set((obj['type'] for obj in all_json))
+items = {}
 
-# Most things have an "id" they use as keys. This holds overrides:
-id_key_exceptions = {"BULLET_PULLING": "bullet",
-                     "MONSTER_FACTION": "name",
-                     "construction": None,
-                     "dream": None,
-                     "hint": None,
-                     "lab_note": None,
-                     "mapgen": None,
-                     "material": "ident",
-                     "monstergroup": "name",
-                     "profession": "ident",
-                     "recipe": None, # technically, "result", but it's non-unique
-                     "scenario": "ident",
-                     "skill": "ident",
-                     "snippet": None,
-                     "speech": None,
-                     "start_location": "ident",
-                     "tutorial_messages": None}
-
-
-item_types = ["AMMO", "ARMOR", "BIONIC_ITEM", "BOOK", "COMESTIBLE", "CONTAINER", "GENERIC",
-              "GUN", "GUNMOD", "ITEM_CATEGORY", "TOOL", "TOOL_ARMOR", "VAR_VEH_PART"]
-
-json = {}
-
-for t in object_types:
-    key_name = 'id'
-    if t in id_key_exceptions:
-        if id_key_exceptions[t] is None:
-            continue # Don't even bother with these guys
-        key_name = id_key_exceptions[t]
-
-    json[t] = { obj[key_name]: obj for obj in all_json if obj['type'] == t and key_name in obj }
-
-    if not json[t]:
-        print("WARN: Can't make sense of these", t, "objects. Here's what one looks like:",
-              repr((obj for obj in all_json if obj['type'] == t).next()))
-
+for otype in item_otypes:
+    if otype not in objects:
+        continue
+    items.update({ item.itype_id: item for item in objects[otype]})
--- parse.py
+++ parse.py
@@ -1,12 +1,42 @@
 import os
 import json
 
+# What object types do we understand?
+# This is an exhaustive list, as far as the library is concerned: if it's not
+# here, it doesn't exist.
+object_types = ['AMMO', 'ARMOR', 'BIONIC_ITEM', 'BOOK', 'BULLET_PULLING',
+                'COMESTIBLE', 'CONTAINER', 'GENERIC', 'GUN', 'GUNMOD',
+                'ITEM_CATEGORY', 'MONSTER', 'MONSTER_FACTION', 'SPECIES',
+                'TOOL', 'TOOL_ARMOR', 'VAR_VEH_PART', 'ammunition_type',
+                'bionic', 'construction', 'effect_type', 'faction',
+                'furniture', 'item_action', 'item_group', 'martial_art',
+                'material', 'monstergroup', 'mutation', 'npc',
+                'overmap_special', 'overmap_terrain', 'profession', 'recipe',
+                'recipe_category', 'region_settings', 'scenario', 'skill',
+                'start_location', 'technique', 'terrain', 'tool_quality',
+                'trap', 'vehicle', 'vehicle_part']
+
+
+# Most things have an "id" they use as keys. This holds overrides:
+id_key_exceptions = {'BULLET_PULLING': 'bullet',
+                     'MONSTER_FACTION': 'name',
+                     'construction': None,
+                     'material': 'ident',
+                     'monstergroup': 'name',
+                     'profession': 'ident',
+                     'recipe': None, # technically, 'result', but it's non-unique
+                     'scenario': 'ident',
+                     'skill': 'ident',
+                     'start_location': 'ident'}
+
+
 def json_files(path='.'):
     jsons = []
     for root, dirs, files in os.walk(path, followlinks=True):
         jsons.extend((os.path.join(root, f) for f in files if f.endswith('.json')))
 
     return jsons
+
 
 def read_full_json(path):
     all_in_one = []
@@ -15,3 +45,50 @@ def read_full_json(path):
             all_in_one.extend(json.load(fp))
 
     return all_in_one
+
+
+def categorize(all_json):
+    categorized_json = {}
+    for t in object_types:
+        categorized_json[t] = [obj for obj in all_json if obj['type'] == t]
+
+    return categorized_json
+
+
+def group(categorized_json):
+    grouped_json = {}
+    for t in categorized_json.keys():
+        key_name = 'id'
+        if t in id_key_exceptions:
+            if id_key_exceptions[t] is None:
+                continue # Explicitly marked as "do not group"
+            key_name = id_key_exceptions[t]
+
+        grouped_json[t] = { obj[key_name]: obj for obj in categorized_json[t] if key_name in obj }
+
+    return grouped_json
+
+
+def categorize_and_group(all_json):
+    return group(categorize(all_json))
+
+
+def value_from(json, key, default=None):
+    value = default
+    if key in json:
+        value = json[key]
+    return value
+
+def require_all(json, keys):
+    """ Require that the given dict-from-json-object contains all given keys """
+    for k in keys:
+        if k not in json:
+            return False
+    return True
+
+def require_any(json, keys):
+    """ Require that the given dict-from-json-object contains at least one of the given keys. """
+    for k in keys:
+        if k in json:
+            return True
+    return False
